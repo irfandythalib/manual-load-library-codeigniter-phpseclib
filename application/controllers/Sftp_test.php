f69cec5d13b43d94b6a397ef9414de76882ba78a
Irfandy Thalib <irfandythalib@gmail.com>
2023-12-04 15:08:43 +0800

    Update Sftp_test.php

--- application/controllers/Sftp_test.php
+++ application/controllers/Sftp_test.php
@@ -1,1 +1,33 @@
+<?php
 
+class Sftp_test extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+
+    }
+
+	function index() {
+		require_once APPPATH . 'third_party/phpseclib/phpseclib/Net/SSH2.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Base.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/RC4.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Rijndael.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Twofish.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Blowfish.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/DES.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/TripleDES.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Random.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/Hash.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/RC2.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Crypt/AES.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Math/BigInteger.php';
+        require_once APPPATH . 'third_party/phpseclib/phpseclib/Net/SFTP.php';
+
+        $sftp = new \phpseclib\Net\SFTP('https://yoursftpdestination');
+		if ($sftp->login('yourusername', 'yourpassword')) {
+			echo "Success connect using SFTP";
+		}
+	}
+}
+?>
